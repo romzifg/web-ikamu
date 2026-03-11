b10490242d4ed0e0906fc29b97609b44a817b377
Romzi Farhan Ghozi <romzifg@gmail.com>
2026-03-12 03:39:32 +0700

    update: fixing variable name in Divider

--- components/ui/Divider.tsx
+++ components/ui/Divider.tsx
@@ -1,5 +1,5 @@
-import { N } from "@/lib/palette";
+import { Neutral } from "@/lib/palette";
 
 export default function Divider({ dark }: { dark: boolean }) {
-  return <div className="w-full h-px" style={{ background: dark ? N[800] : N[200] }} />;
+  return <div className="w-full h-px" style={{ background: dark ? Neutral[800] : Neutral[200] }} />;
 }
